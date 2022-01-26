6c1e05853c620da651599f671c059eaef6ef556c
lmkim06 <98493430+lmkim06@users.noreply.github.com>
2022-01-26 17:43:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def msg_received_from_group():
   
   #Check the text of the message sent to the chat to see if it matches our command word
   if data['text'].lower() == "hello":
-    send_msg("hi")
+    send_msg("hello")
 	
 
   return "ok", 1
